37dcab7dea8b9065aa7b0ac77c413149ecd38093
LukeySan <116930526+LukeySan@users.noreply.github.com>
2025-02-12 01:04:08 -0500

    build.sh file for deployment

--- risk-analyst/backend/build.sh
+++ risk-analyst/backend/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
+
 cd risk_analyst
 mkdir -p staticfiles
-python3 manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
+cd ..
